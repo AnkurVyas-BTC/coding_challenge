211b8863b3194e77876648c8964e1f38bff48ba6
JigarShahBtpl <jigar.shah@botreetechnologies.com>
2020-02-06 13:40:00 +0530

    model made for pageview and visit and required it to app.rb

--- application_challenge/app.rb
+++ application_challenge/app.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
+# require sinatra and active record here
 require 'sinatra'
 require 'sinatra/activerecord'
 
+# require models here
+require './model/pageview'
+require './model/visit'
+
+# database configuration
 set :database_file, 'database.yml'
---
+++ application_challenge/model/pageview.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Pageview < ActiveRecord::Base
+end
